58b0131b54c19eb6e6af34cdf77055c437ef1b26
Tommy <tommy128works@gmail.com>
2023-07-27 20:04:31 -0400

    feat: add gameboard UI
    Created 10x10 gameboard for player and computer.

---
+++ src/js/gameboardUI.js
@@ -0,0 +1,30 @@
+const BOARD_SIZE = 10;
+
+const createGameboard = (id) => {
+  let container = document.createElement("div");
+  // container.setAttribute("id", id);
+
+  let name = document.createElement("h1");
+  name.textContent = id.toUpperCase();
+  container.appendChild(name);
+
+  let gameboardContainer = document.createElement("div");
+  gameboardContainer.setAttribute("id", id + "-gameboard-container");
+  gameboardContainer.classList.add("game-board-container");
+
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      let tile = document.createElement("div");
+      tile.dataset.x = j;
+      tile.dataset.y = 7 - i;
+      gameboardContainer.appendChild(tile);
+    }
+  }
+
+
+  container.appendChild(gameboardContainer);
+
+  return container;
+}
+
+export default createGameboard;
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+
+import createGameboard from "./gameboardUI";
+
+let contentContainer = document.createElement("div");
+contentContainer.setAttribute("id", "content-container");
+
+contentContainer.appendChild(createGameboard("player"));
+contentContainer.appendChild(createGameboard("computer"));
+
+document.body.appendChild(contentContainer);
